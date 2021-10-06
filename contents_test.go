b9eb1e099cf87eb0ca9c3d7dfa94d50f6322c5fa
Alexander F. Rødseth <rodseth@gmail.com>
2021-10-06 12:07:17 +0200

    Let the content detection take an initial mode

--- contents_test.go
+++ contents_test.go
@@ -1,11 +1,10 @@
 package mode
 
 import (
-	"strings"
 	"testing"
 )
 
-func TestDetectFromContents(t *testing.T) {
+func TestSimpleDetect(t *testing.T) {
 	examples := map[string]Mode{
 		"#!/bin/bash\necho Hello\n":              Shell,
 		"#!/usr/bin/env python\nprint(\"hi\")\n": Python,
@@ -14,7 +13,7 @@ func TestDetectFromContents(t *testing.T) {
 		"#include <stdio.h>":                     Blank, // hard to tell if this is C or C++, should be detected by filename, not by contents
 	}
 	for s, targetMode := range examples {
-		if m, _ := DetectFromContents(strings.Split(s, "\n")[0], func() string { return s }); m != targetMode {
+		if m := SimpleDetect(s); m != targetMode {
 			t.Fatalf("Expected %s got %s", targetMode.String(), m.String())
 		}
 	}
